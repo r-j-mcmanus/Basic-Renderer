170789dbfd6fb7936714e5b84c1fc13a47adbc32
Ryan Mcmanus <35876565+r-j-mcmanus@users.noreply.github.com>
2025-08-01 11:46:03 +0100

    start of audio stuff

--- Errors.h
+++ Errors.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <GL/glew.h>
+#include <AL/al.h>
+
+#include <iostream>
+
 
 // macros for error logging openGL
 // #x returns the litteral string eg #h = "foo"
@@ -18,3 +22,16 @@ void GLClearError();
 CleLogs any errors in the GL error buffer
 */
 bool GLLogCall(const char* function, const char* file, int line);
+
+#define OpenAL_ErrorCheck(message)\
+{\
+    ALenum error = glGetError();\
+    if (error != AL_NO_ERROR)\
+    {\
+        std::cerr << "OpenAL Error: " << error << std::endl;\
+    }\
+}
+
+#define ALCall(x)\
+x;\
+OpenAL_ErrorCheck(x);
